86b38cd9c0bddeeef7614e302f0cc2b6cd193c72
Yanush Viktar <vicjan89@gmail.com>
2023-01-18 22:06:49 +0300

    Create superuser

--- EnergoProcess/EnergoProcess/settings.py
+++ EnergoProcess/EnergoProcess/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    # 'tabel.apps.TabelConfig',
+    'tabel.apps.TabelConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- EnergoProcess/tabel/models.py
+++ EnergoProcess/tabel/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class Person(models.Model):
-        name = models.CharField(max_length=40)
+    name = models.CharField(max_length=40)
     position = models.CharField(max_length=40)
     electrical_safety_group = models.CharField(max_length=3)
