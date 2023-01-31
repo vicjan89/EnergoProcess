57ba2689ea6fae99012e092d0525d50dba5d6eff
Yanush Viktar <vicjan89@gmail.com>
2023-01-31 23:16:17 +0300

    Form select tabel work. Fixed vertical align page.

--- EnergoProcess/tabel/views.py
+++ EnergoProcess/tabel/views.py
@@ -16,9 +16,12 @@ context = {'title': 'Табель бригады',
            'brigades': list(set((i.supervisor for i in Brigades.objects.all()))),
            'person': Person}
 
+month_by_number = {'1': 'январь', '2': 'февраль', '3': 'март', '4': 'апрель', '5': 'май', '6': 'июнь',
+                   '7': 'июль', '8': 'август', '9': 'сентябрь', '10': 'октябрь', '11': 'ноябрь', '12': 'декабрь'}
 def tabel(request):
     supervisor_id = request.POST.get("supervisor_id", 1)
-    month = request.POST.get("month", 1)
+    month = request.POST.get("month", '1')
+    context['month'] = month_by_number[month]
     context['supervisor'] = Person.objects.get(pk=supervisor_id)
     brigada = [i.member for i in Brigades.objects.filter(supervisor=supervisor_id)]
     tabel_filtred = TabelRecord.objects.filter(master=supervisor_id).filter(date_work__month=month).filter(transferred=None)
@@ -31,6 +34,11 @@ def tabel(request):
         tabel_dict[b.name].append(b.personnel_number)
         tabel_dict[b.name].append(0.0)
     for i in tabel_filtred:
+        if i.person.name not in tabel_dict:
+            tabel_dict[i.person.name] = [' ' for i in range(31)]
+            tabel_dict[i.person.name].append(i.person.position)
+            tabel_dict[i.person.name].append(i.person.personnel_number)
+            tabel_dict[i.person.name].append(0.0)
         tabel_dict[i.person.name][i.date_work.month] = str(i)
         tabel_dict[i.person.name][33] += i.work_time if i.work_time else 0.0
     for key, value in tabel_dict.items():
