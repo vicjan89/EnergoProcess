5ba09d4f15e901c67d2764f817f4707a66f36d7f
Yanush Viktar <vicjan89@gmail.com>
2023-02-23 09:39:29 +0300

    add filter by decade current month in  TabelRecordAdmin

--- EnergoProcess/tabel/admin.py
+++ EnergoProcess/tabel/admin.py
@@ -1,4 +1,8 @@
+import datetime
+
 from django.contrib import admin
+from django.contrib.admin import SimpleListFilter
+from django.utils.translation import gettext_lazy as _
 
 from .models import *
 
@@ -33,24 +37,36 @@ def make_on_vacation(modeladmin, request, queryset):
     queryset.update(work_time=None, work_type=WorkType.objects.get(work_type='О'), work_foreman=False, harmfulness=False,
                     siding=False, combination=None, transferred=None)
 
+class ScrapeStatusFilter(SimpleListFilter):
+    title = "декады месяца"  # a label for our filter
+    parameter_name = "decade"  # you can put anything here
+
+    def lookups(self, request, model_admin):
+        return (
+            ('d_1', _('1 декада')),
+            ('d_2', _('2 декада')),
+            ('d_3', _('3 декада')),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == "d_1":
+            return queryset.filter(date_work__month=datetime.date.today().month).filter(date_work__day__lte=10)
+        if self.value() == "d_2":
+            return queryset.filter(date_work__month=datetime.date.today().month).filter(date_work__day__lte=20).filter(date_work__day__gt=10)
+        if self.value() == "d_3":
+            return queryset.filter(date_work__month=datetime.date.today().month).filter(date_work__day__gt=20)
+
 class TabelRecordAdmin(admin.ModelAdmin):
-    list_display = ('date_work', 'master', 'person', 'work_time', 'work_type', 'work_foreman',
-                    'harmfulness', 'siding', 'transferred')
+    list_display = ('date_work', 'person', 'work_time', 'work_type', 'work_foreman',
+                    'harmfulness', 'siding', 'transferred', 'master',)
     list_display_links = ('date_work',)
     list_editable = ('master', 'person', 'work_time', 'work_type', 'work_foreman',
                     'harmfulness', 'siding', 'transferred')
     search_fields = ('date_work', 'person')
-    list_filter = ('date_work', 'master', 'person')
+    list_filter = ('date_work', ScrapeStatusFilter, 'master', 'person')
     actions = [make_sick, make_on_vacation]
     save_as = True
-    save_on_top = True
 
-
-    def get_queryset(self, request):
-        qs = super().get_queryset(request)
-        if request.user.is_superuser:
-            return qs
-        return qs.filter(master__name__startswith=request.user.get_full_name().split()[1])
 
 admin.site.register(TabelRecord, TabelRecordAdmin)
 
